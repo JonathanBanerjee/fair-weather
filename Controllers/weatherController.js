f4a1d6c0c972ef449c00cb16b1e68214081d7ed7
jonathanbanerjee <jonathan@youd.net>
2024-02-06 16:47:10 +0000

    adding more to api

--- Controllers/weatherController.js
+++ Controllers/weatherController.js
@@ -6,6 +6,13 @@ export async function getApiData(coords) {
     const { data } = await axios.get(
       `${API_URL}/data/2.5/forecast?lat=${coords.latitude}&lon=${coords.longitude}&appid=${API_KEY}`
     );
+
+    if (data && data.city) {
+      const cityName = data.city.name;
+      cityRef.innerHTML = `<h2> ${cityName}</h2>`;
+    } else {
+      cityRef.innerHTML = `<h2>Location name not available</h2>`;
+    }
   } catch (error) {
     console.error(error);
   }
