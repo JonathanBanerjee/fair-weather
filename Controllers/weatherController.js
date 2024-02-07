3dd175154816b0d70994344bc47c26a0d043f881
jonathanbanerjee <jonathan@youd.net>
2024-02-07 17:57:59 +0000

    adding api calls

--- Controllers/weatherController.js
+++ Controllers/weatherController.js
@@ -1,17 +1,17 @@
-import { API_KEY, API_URL } from "../config";
-import { getLocation } from "./getLocationController";
+import { API_KEY, API_URL } from "../config.js";
 
-export async function getApiData(coords) {
+export async function getWeather(coords) {
   try {
     const { data } = await axios.get(
       `${API_URL}/data/2.5/forecast?lat=${coords.latitude}&lon=${coords.longitude}&appid=${API_KEY}`
     );
 
-    if (data && data.city) {
-      const cityName = data.city.name;
-      cityRef.innerHTML = `<h2> ${cityName}</h2>`;
-    } else {
-      cityRef.innerHTML = `<h2>Location name not available</h2>`;
+    if (data) {
+      console.log(data.list[2].weather);
+      console.log(data.list[2].weather[0].description);
+      console.log(data.list[8].weather);
+      console.log(data.list[8].weather[0].description);
+      // return data.list.weather;
     }
   } catch (error) {
     console.error(error);
--- index.js
+++ index.js
@@ -1,2 +1,34 @@
 import { getLocation } from "./Controllers/getLocationController.js";
-getLocation();
+import { getWeather } from "./Controllers/weatherController.js";
+import { getSunrise, getSunset } from "./Controllers/sunController.js";
+import { getCity } from "./Controllers/getCityController.js";
+
+const coordsRef = document.getElementById("coords");
+const cityRef = document.getElementById("city");
+const weatherRef = document.getElementById("weather");
+const button = document.getElementById("getLocation");
+
+button.addEventListener("click", () => clickHandler());
+
+const clickHandler = async () => {
+  try {
+    const coords = await getLocation();
+    const city = await getCity(coords);
+    const weather = await getWeather(coords);
+    const sunrise = await getSunrise(coords);
+    const sunset = await getSunset(coords);
+    cityRef.innerHTML = city;
+    // weatherRef.innerHTML = weather;
+    console.log(weather);
+    console.log(sunrise);
+    console.log(sunset);
+  } catch (error) {
+    console.log(error, "reason");
+  }
+};
+
+// const searchHandler = async (city) => {
+//   try {
+//     cityRef.innerHTML = city;
+//   } catch (error) {}
+// };
