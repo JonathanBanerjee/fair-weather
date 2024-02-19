9ce6904d094caea1360fc26488b73d8646feb8c6
jonathanbanerjee <jonathan@youd.net>
2024-02-19 18:46:03 +0000

    adding space

--- Controllers/searchLocationController.js
+++ Controllers/searchLocationController.js
@@ -1,15 +1,17 @@
-// const CITY_URL = `http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}`;
-// import { API_KEY } from "../config";
+import { API_KEY } from "../config.js";
 const cityList = document.getElementById("city-list");
 export const inputHandler = async (userInput) => {
   console.log(userInput);
 
   try {
     const { data } = await axios.get(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${userInput}&limit=5&appid=7e0d6928eed4d376da50312f8d85e6ca`
+      `http://api.openweathermap.org/geo/1.0/direct?q=${userInput}&limit=5&appid=${API_KEY}`
     );
     console.log(data);
-    const list = data.map((item) => `<li>${item.name}</li>`);
+    const list = data.map(
+      (item) =>
+        `<li>${item.name + ", " + item.state + ", " + item.country} </li>`
+    );
     const joinedList = list.join("");
     cityList.innerHTML = joinedList;
   } catch (error) {
